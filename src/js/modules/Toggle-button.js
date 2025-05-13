f8f50d7a031635edf1467d45e70fd0861e4164c2
guilhermesiqueira13 <guilherme.nunes13@fatec.sp.gov.br>
2025-05-13 17:23:37 -0300

    Nova página adicionada: termos.html, estrutura e visual prontos

--- src/js/modules/Toggle-button.js
+++ src/js/modules/Toggle-button.js
@@ -1,11 +1,9 @@
 export class ToggleButton {
   constructor() {
-    // Seleciona os botões de toggle e os elementos de preço/período
     this.toggleButtons = document.querySelectorAll(".toggle-btn");
     this.priceElements = document.querySelectorAll(".card-highlight-number");
     this.periodElements = document.querySelectorAll(".card-highlight-info");
 
-    // Define os preços para cada plano (mensal e anual)
     this.prices = {
       mensal: {
         basico: "R$49",
@@ -19,7 +17,6 @@ export class ToggleButton {
       },
     };
 
-    // Garante que os elementos necessários foram encontrados
     if (
       this.toggleButtons.length === 2 &&
       this.priceElements.length === 3 &&
@@ -33,10 +30,8 @@ export class ToggleButton {
     }
   }
 
-  // Inicializa o toggle com o plano mensal e adiciona eventos
   init() {
     this.updatePrices("mensal");
-    // Define o estado inicial do botão "Mensal" como selecionado
     const mensalButton = document.querySelector(
       '.toggle-btn[data-period="mensal"]'
     );
@@ -46,24 +41,19 @@ export class ToggleButton {
     this.bindEvents();
   }
 
-  // Adiciona eventos de clique aos botões de toggle
   bindEvents() {
     this.toggleButtons.forEach((button) => {
       button.addEventListener("click", () => {
         const period = button.dataset.period;
         this.setSelectedButton(button);
         this.updatePrices(period);
-        // Depuração: Verifica qual período foi selecionado
         console.log("Período selecionado:", period);
       });
     });
   }
 
-  // Atualiza os preços e o texto do período com base no período selecionado
   updatePrices(period) {
-    // Verifica se o período é válido
     if (this.prices.hasOwnProperty(period)) {
-      // Mapeia os índices dos elementos de preço aos nomes dos planos
       const planOrder = ["basico", "premium", "empresarial"];
       this.priceElements.forEach((element, index) => {
         const plan = planOrder[index];
@@ -75,8 +65,6 @@ export class ToggleButton {
           );
         }
       });
-
-      // Atualiza o texto do período
       const periodText = period === "mensal" ? "por mês" : "por ano";
       this.periodElements.forEach((element) => {
         element.textContent = periodText;
@@ -86,13 +74,11 @@ export class ToggleButton {
     }
   }
 
-  // Gerencia o estado selecionado dos botões de toggle
   setSelectedButton(selectedButton) {
     this.toggleButtons.forEach((button) => {
       button.classList.remove("selected");
     });
     selectedButton.classList.add("selected");
-    // Depuração: Verifica qual botão foi selecionado
     console.log(
       "Botão selecionado:",
       selectedButton.textContent,
